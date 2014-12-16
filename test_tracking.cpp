27617d3f0041b42be3b35f4783a5427d80197cfd
David Held <davheld@cs.stanford.edu>
2014-12-15 22:07:14 -0800

    Creating sensor_spec header

--- test_tracking.cpp
+++ test_tracking.cpp
@@ -15,6 +15,7 @@
 #include <precision_tracking/track_manager_color.h>
 #include <precision_tracking/tracker.h>
 #include <precision_tracking/high_res_timer.h>
+#include <precision_tracking/sensor_specs.h>
 
 using std::string;
 
@@ -204,36 +205,6 @@ void find_bad_frames(const precision_tracking::track_manager_color::TrackManager
   }
 }
 
-// Computes the sensor resolution for an object at a given distance,
-// for the 64-beam Velodyne.
-void getSensorResolution(const Eigen::Vector3f& centroid_local_coordinates,
-                         double* sensor_horizontal_res,
-                         double* sensor_vertical_res) {
-  // Get the distance to the tracked object.
-  const double horizontal_distance =
-      sqrt(pow(centroid_local_coordinates(0), 2) +
-           pow(centroid_local_coordinates(1), 2));
-
-  // The horizontal resolution for the 64-beam Velodyne spinning at 10 Hz
-  // is 0.18 degrees.
-  const double velodyne_horizontal_angular_res = 0.18;
-
-  // There are 64 beams spanning 26.8 vertical degrees, so the average spacing
-  // between each beam is computed as follows.
-  const double velodyne_vertical_angular_res = 26.8 / 63;
-
-  // We convert the angular resolution to meters for a given range.
-  const double velodyne_horizontal_res =
-      2 * horizontal_distance *
-      tan(velodyne_horizontal_angular_res / 2.0 * pi / 180.0);
-  const double velodyne_vertical_res =
-      2 * horizontal_distance *
-      tan(velodyne_vertical_angular_res / 2.0 * pi / 180.0);
-
-  *sensor_horizontal_res = velodyne_horizontal_res;
-  *sensor_vertical_res = velodyne_vertical_res;
-}
-
 void track(
            const precision_tracking::track_manager_color::TrackManagerColor& track_manager,
            const precision_tracking::Params& params,
@@ -294,8 +265,9 @@ void track(
       // Get the sensor resolution.
       double sensor_horizontal_resolution;
       double sensor_vertical_resolution;
-      getSensorResolution(frame->getCentroid(), &sensor_horizontal_resolution,
-                          &sensor_vertical_resolution);
+      precision_tracking::getSensorResolution(
+            frame->getCentroid(), &sensor_horizontal_resolution,
+            &sensor_vertical_resolution);
 
       // Track object.
       Eigen::Vector3f estimated_velocity;
